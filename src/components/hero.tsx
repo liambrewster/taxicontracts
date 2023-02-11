73b507e3228187348a869ca3fd37bf4c7843250d
Liam Brewster <liam.brewster@hotmail.co.uk>
2023-02-11 15:35:52 +0000

    feat: added the header nav

---
+++ src/components/hero.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+function Hero() {
+  return (
+    <header className="body-font text-gray-600">
+      <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
+        <a className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 rounded-full bg-yellow-500 p-2"
+            viewBox="0 0 576 512"
+          >
+            <path d="M352 0C369.7 0 384 14.33 384 32V64L384 64.15C422.6 66.31 456.3 91.49 469.2 128.3L504.4 228.8C527.6 238.4 544 261.3 544 288V480C544 497.7 529.7 512 512 512H480C462.3 512 448 497.7 448 480V432H128V480C128 497.7 113.7 512 96 512H64C46.33 512 32 497.7 32 480V288C32 261.3 48.36 238.4 71.61 228.8L106.8 128.3C119.7 91.49 153.4 66.31 192 64.15L192 64V32C192 14.33 206.3 0 224 0L352 0zM197.4 128C183.8 128 171.7 136.6 167.2 149.4L141.1 224H434.9L408.8 149.4C404.3 136.6 392.2 128 378.6 128H197.4zM128 352C145.7 352 160 337.7 160 320C160 302.3 145.7 288 128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352zM448 288C430.3 288 416 302.3 416 320C416 337.7 430.3 352 448 352C465.7 352 480 337.7 480 320C480 302.3 465.7 288 448 288z" />
+          </svg>
+          <span className="ml-3 text-xl">Taxi Contracts</span>
+        </a>
+        <Link
+          href="/contracts"
+          className="flex flex-wrap items-center justify-center text-base md:ml-auto"
+        >
+          <nav>
+            <a className="mr-5 hover:text-gray-900">View Available Contracts</a>
+          </nav>
+        </Link>
+        <Link href="/signin">
+          <button className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 py-1 px-3 text-base hover:bg-gray-200 focus:outline-none md:mt-0">
+            Operator Sign In
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="ml-1 h-4 w-4"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Hero;
