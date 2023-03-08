c1bf1f8098db3094ebc8460e0a73c6f321eb30bb
Liam Brewster <liam.brewster@hotmail.co.uk>
2023-03-08 00:46:42 +0000

    refactor: get blank state to show some placeholder dataon the bid tabe

--- src/components/bidtable.tsx
+++ src/components/bidtable.tsx
@@ -7,6 +7,7 @@ interface contractItem {
   id: string;
   internalId: string;
   operatorId: string;
+  operator: { name: string };
   status: string;
   pickup: string;
   destination: string;
@@ -21,8 +22,32 @@ interface contractItem {
 interface contractItems extends Array<contractItem> {}
 
 function BidTable() {
-  const { data: contracts } = api.contract.getAll.useQuery();
-
+  const [contracts, setContracts] = useState<contractItems>([
+    {
+      id: "123",
+      internalId: "No Contracts Available",
+      operatorId: "",
+      operator: { name: "" },
+      status: "NODATA",
+      pickup: "😢",
+      destination: "😢",
+      collections: 0,
+      distance: 0,
+      vehicleSize: 0,
+      timings: "😢",
+      days: "😢",
+      journeytype: "😢",
+    },
+  ]);
+  const { data, isLoading, error } = api.contract.getAll.useQuery();
+  useEffect(() => {
+    if (isLoading) {
+      console.log("Loading Data");
+    }
+    if (data) {
+      setContracts(data);
+    }
+  }, []);
   return (
     <div className="mx-2 overflow-x-auto rounded-lg border border-gray-200 shadow-md md:mx-10">
       <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
@@ -71,14 +96,14 @@ function BidTable() {
                 <div className="relative h-10 w-10">
                   <img
                     className="h-full w-full rounded-full object-cover object-center"
-                    src={`https://eu.ui-avatars.com/api/?name=${contract.operator.name}&size=250`}
+                    src={`https://eu.ui-avatars.com/api/?name=${contract.operator?.name}&size=250`}
                     alt=""
                   />
                   <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
                 </div>
                 <div className="text-sm">
                   <div className="font-medium text-gray-700">
-                    {contract.operator.name}
+                    {contract.operatorId ? contract.operator.name : ""}
                   </div>
                   <div className="text-xs text-gray-400">
                     Ref: {contract.internalId}
@@ -146,85 +171,5 @@ const Stage = ({ label }: { label: string }) => {
       <span className={`h-1.5 w-1.5 rounded-full ${backgroundColour}`}></span>
       {text}
     </span>
-  );
-};
-
-const blankData = () => {
-  return (
-    <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
-      <thead className="bg-gray-50">
-        <tr>
-          <th scope="col" className=" px-6 py-4 font-medium text-gray-900">
-            Operator
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Status
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Type
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Pickup
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Drop-off
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Pickups
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Est Mileage
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Vehicle Size
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            Timings
-          </th>
-
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-            No of Days
-          </th>
-          <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-        <tr className="hover:bg-gray-50">
-          <th className=" flex gap-3 px-6 py-4 font-normal text-gray-900">
-            <div className="relative h-10 w-10">
-              <img
-                className="h-full w-full rounded-full object-cover object-center"
-                src={`https://eu.ui-avatars.com/api/?name=No+Data&size=250`}
-                alt=""
-              />
-              <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
-            </div>
-            <div className="text-sm">
-              <div className="font-medium text-gray-700">
-                No Current Contracts
-              </div>
-              <div className="text-xs text-gray-400">Ref:</div>
-            </div>
-          </th>
-          <td className="px-6 py-4">
-            <Stage label="NODATA" />
-          </td>
-          <td className="px-6 py-4">NONE</td>
-          <td className="px-6 py-4">😢</td>
-          <td className="px-6 py-4">😢</td>
-          <td className="px-6 py-4">0</td>
-          <td className="px-6 py-4">0</td>
-          <td className="px-6 py-4">0</td>
-          <td className="px-6 py-4">0</td>
-          <td className="px-6 py-4">😢</td>
-          <td className="px-6 py-4">
-            😢
-            <div className="flex justify-end gap-4">
-              <RiAuctionLine size="28" />
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   );
 };
